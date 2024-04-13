25b9ced2997d5d633c61c1523aff356ec4534b0d
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-04-13 22:29:25 +0530

    some setup updates

--- bananabackend/src/index.js
+++ bananabackend/src/index.js
@@ -1,14 +1,22 @@
 // require('dotenv').config({ path: './.env' });
 import dotenv from "dotenv"
-
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env'
 });
+const port = process.env.PORT || 8000;
+connectDB()
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+})
+.catch((err) => {
+    console.log("Error connecting to the database", err);
+})
 
-
-connectDB();
 
 
 
