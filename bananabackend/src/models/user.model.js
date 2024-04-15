905cc6c91afcfd29ec73aa34ec59e25923b2a65b
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-04-15 23:21:11 +0530

    model update

--- bananabackend/src/models/user.model.js
+++ bananabackend/src/models/user.model.js
@@ -1,4 +1,5 @@
-import { Avatar } from "@material-ui/core";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema(
     {
@@ -17,7 +18,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
@@ -49,5 +50,40 @@ const userSchema = new Schema(
     },
 );
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next();
+})
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User = mongoose.model("User", userSchema);
