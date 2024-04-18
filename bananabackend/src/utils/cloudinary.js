c9f9ad8d332daefdcf94fabf83e4de9885d3e6a0
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-04-18 22:23:24 +0530

    minor

--- bananabackend/src/utils/cloudinary.js
+++ bananabackend/src/utils/cloudinary.js
@@ -10,4 +10,4 @@ cloudinary.config({
 
 cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+  function(error, result) {console.log(result);});
